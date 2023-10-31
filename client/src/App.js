0aac4fc420fe550d73955b35d757bd13f3bcc910
Thang Nguyen <thang.nd194169@sis.hust.edu.vn>
2023-10-31 19:25:48 +0700

    feat: add route login register

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import Pagenotfound from './pages/Pagenotfound';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/" element={<Pagenotfound/>} />
-
+        <Route path="/" element={<HomePage />} />
+        <Route path="/404" element={<Pagenotfound />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
