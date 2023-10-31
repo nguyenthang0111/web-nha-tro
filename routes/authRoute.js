7e457de3ccac80e79f9751402b317cd4711eb000
Thang Nguyen <thang.nd194169@sis.hust.edu.vn>
2023-10-31 18:30:16 +0700

    feat: first commit

---
+++ routes/authRoute.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import { loginController, registerController, testController } from '../controllers/authController.js'
+
+//router object 
+const router = express.Router()
+
+//routing 
+//REGISTER || METHOD POST 
+router.post('/register', registerController)
+
+// LOGIN || POST 
+router.post('/login', loginController)
+
+// test routes
+router.get('/test', testController)
+
+export default router   
