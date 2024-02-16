46f1a13953eaa6c9a318598b00b08f12d2acc974
Thang Nguyen <thang.nd194169@sis.hust.edu.vn>
2024-02-16 11:09:10 +0700

    feat: fe add filter

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Layout from '../components/Layout/Layout'
 import { useAuth } from '../context/auth'
 import RoomCard from '../components/RoomCard';
+import { districts } from '../lib/consts/feconst';
 
 
 function HomePage() {
@@ -49,11 +50,55 @@ function HomePage() {
   return (
     <Layout title="Trang chủ">
       {/* Banner */}
-      <div className='mb-28'>
+      <div className='mb-28 static'>
         <img className='w-full h-full bg-no-repeat bg-top bg-cover object-cover' src="/images/homepage.jpg" alt="" />
+        {/* Box Filter */}
+        <div className='absolute bottom-80 left-60 bg-[#140C40] pt-[47px] pl-[50px] w-[869px]'>
+          <div>
+            <h1 className='text-[56px] font-normal text-white mb-2.5 leading-[1.2]'>Find Your Dream Room</h1>
+            <p className='text-xl text-[#DEDEDE] mb-[60px] '>We Have Over Million Properties For You</p>
+            <div>
+              <button className='bg-white text-[#0FB45F] text-sm px-12 py-3.5 uppercase'>Bộ lọc</button>
+            </div>
+          </div>
+        </div>
+
+        <div className='absolute bottom-52 left-60 bg-white py-[35px] px-[30px] w-[819px] ml-[50px]'>
+          <form className='flex place-content-between'>
+            <select id="countries" class="bg-gray-50 border w-[27%] border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <option selected>Chọn quận</option>
+              {districts.map((district) => (
+                <option>{district}</option>
+              ))}
+              {/* <option value="US">United States</option> */}
+            </select>
+
+            <select id="countries" class="bg-gray-50 border w-[27%] border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <option selected>Chọn giá trị phòng</option>
+              <option>Dưới 1 triệu 5</option> 
+              <option>Từ 1 triệu 5 đến 2 Triệu 5</option> 
+              <option>Từ 1 triệu 5 đến 2 Triệu 5</option> 
+              <option>Từ 2 triệu 5 đến 3 Triệu 5</option> 
+              <option>Từ 3 triệu 5 đến 4 Triệu 5</option> 
+              <option>Từ 4 triệu 5 trở lên</option> 
+            </select>
+
+            <select id="countries" class="bg-gray-50 border w-[27%] border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <option selected>Chọn giá điện</option>
+              <option>Dưới 1 triệu 5</option> 
+              <option>Từ 1 triệu 5 đến 2 Triệu 5</option> 
+              <option>Từ 1 triệu 5 đến 2 Triệu 5</option> 
+              <option>Từ 2 triệu 5 đến 3 Triệu 5</option> 
+              <option>Từ 3 triệu 5 đến 4 Triệu 5</option> 
+              <option>Từ 4 triệu 5 trở lên</option> 
+            </select>
+
+            <button className='bg-[#0FB45F] text-white py-[10px] px-[20px] text-sm rounded-[50px] uppercase font-medium'>Search</button>
+          </form>
+        </div>
       </div>
 
-      {/* List Room  */}
+      {/* List Room  */}  
       <div className='list-room'>
         <h3 className='text-3xl font-medium leading-normal mb-5'>Searching for the Best Places?</h3>
         <div className='flex place-content-between flex-wrap'>
